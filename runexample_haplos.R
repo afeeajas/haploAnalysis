e5676a2cc11f9a52a91d2279c6c174d247b5d071
afeeajas <afeezajasa@gmail.com>
2024-07-04 12:46:50 +0200

    Update runexample_haplos.R

--- runexample_haplos.R
+++ runexample_haplos.R
@@ -4,8 +4,8 @@ setwd("")
 source('haplotypeBglV5genogrm.R')
 
 #make custom made haploblocks
-haploblocks <- makehaploblocks()
+haploblocks <- makehaploblocks(mapinfohap='GENOimp.map',nbsize=10000)
 
 ## after phasing your data with beagle extract the haplotypes for each block
-haplos.allele <- makehaplotypes(phasedbgl='/home/afeesa/paper3/data/haploview_dat/GENOimphased_ssa12.vcf.gz.recode.vcf',mapinfohap='/home/afeesa/paper3/data/haploview_dat/GENOimp_filtssa12.map',hapblocks=haploblocks)
+haplos.allele <- makehaplotypes(phasedbgl='GENOphased.vcf',mapinfohap='GENOimp.map',hapblocks=haploblocks)
 
